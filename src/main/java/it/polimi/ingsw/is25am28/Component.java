47789e2c54dbeb69f69e2b0b2dc92872f185dd30
alle0017 <allegretti0017@gmail.com>
2025-03-09 14:40:19 +0100

    interfacce

---
+++ src/main/java/it/polimi/ingsw/is25am28/Component.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.is25am28;
+
+// TODO: This is currently only a stub needed to avoid errors in Ship
+public class Component {
+    private int[] position;
+    private int[] sides;
+
+    // Constructor
+    public Component() {
+
+    }
+
+    //
+    public int[] getComponentPosition() {
+        return this.position;
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module it.polimi.ingsw.is25am28 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens it.polimi.ingsw.is25am28 to javafx.fxml;
+    exports it.polimi.ingsw.is25am28;
+    exports it.polimi.ingsw.is25am28.exceptions;
+    opens it.polimi.ingsw.is25am28.exceptions to javafx.fxml;
+}
