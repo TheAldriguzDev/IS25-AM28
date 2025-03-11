47dd18177f0e2af5fd259f0141968bc2260c7628
alle0017 <allegretti0017@gmail.com>
2025-03-11 11:40:03 +0100

    components

---
+++ src/main/java/it/polimi/ingsw/is25am28/components/Cabin.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.is25am28.components;
+
+public final class Cabin extends Component {
+      public boolean check( Component[] nearest ){
+            return false;
+      }
+}
---
+++ src/main/java/it/polimi/ingsw/is25am28/components/Shield.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.is25am28.components;
+
+public class Shield {
+      
+}
