a362b85d644cbc8f0e753945dd028ff3bc0a786b
nanomachine128 <gabriele.bertirotti@mail.polimi.it>
2025-03-13 08:07:58 +0100

    Fixed many errors, incomplete implementations and many other things to remove all compiler errors (although much more work is still required)
    # Conflicts:
    #	src/main/java/it/polimi/ingsw/is25am28/EventCards/AbandonedStation.java
    #	src/main/java/it/polimi/ingsw/is25am28/EventCards/EventCard.java
    #	src/main/java/it/polimi/ingsw/is25am28/EventCards/Pirates.java
    #	src/main/java/it/polimi/ingsw/is25am28/EventCards/Smugglers.java
    #	src/main/java/it/polimi/ingsw/is25am28/EventCards/Stardust.java
    #	src/main/java/it/polimi/ingsw/is25am28/Ship/Ship.java
    #	src/main/java/it/polimi/ingsw/is25am28/components/Component.java

---
+++ src/main/java/it/polimi/ingsw/is25am28/Connector.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.is25am28;
+
+public enum Connector {
+    ZERO_PIPES,
+    ONE_PIPE,
+    TWO_PIPES,
+    THREE_PIPES,
+}
--- src/main/java/it/polimi/ingsw/is25am28/EventCards/Sabotage.java
+++ src/main/java/it/polimi/ingsw/is25am28/EventCards/Sabotage.java
@@ -1,4 +1,25 @@
 package it.polimi.ingsw.is25am28.EventCards;
 
+import it.polimi.ingsw.is25am28.Player;
+
 public class Sabotage extends EventCard {
+
+    public Sabotage(String name, int cardLevel) {
+        super(name, cardLevel);
+    }
+
+    @Override
+    void useCard(Player[] players) {
+
+    }
+
+    @Override
+    void bonusEffect(Player player) {
+
+    }
+
+    @Override
+    void malusEffect(Player player) {
+
+    }
 }
