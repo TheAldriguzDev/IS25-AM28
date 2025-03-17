281e8c79249a824f25dee2b609b8302f1c390af7
Filippo <filippo.baldissara@outlook.com>
2025-03-17 23:16:11 +0100

    Finished cards (Pirates, Smugglers, Stardust, Slavers)

--- src/main/java/it/polimi/ingsw/is25am28/ActionJSON/PiratesStateJSON.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.is25am28.ActionJSON;
-
-public class PiratesStateJSON extends ActionJSON {
-}
--- src/main/java/it/polimi/ingsw/is25am28/ActionJSON/SlaversStateJSON.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.is25am28.ActionJSON;
-
-public class SlaversStateJSON extends ActionJSON {
-}
