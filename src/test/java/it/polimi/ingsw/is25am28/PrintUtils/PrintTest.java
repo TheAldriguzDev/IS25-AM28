160d9c080f50971253d1b8ff1f6777405b77566a
nanomachine128 <gabriele.bertirotti@mail.polimi.it>
2025-04-05 18:57:14 +0200

    Added all battery TUI test cases

--- src/test/java/it/polimi/ingsw/is25am28/PrintUtils/PrintTest.java
+++ src/test/java/it/polimi/ingsw/is25am28/PrintUtils/PrintTest.java
@@ -18,73 +18,72 @@ public class PrintTest {
     @BeforeEach
     void init() {
         connectors = new ArrayList<>();
+        connectors.add(0);
         connectors.add(1);
         connectors.add(2);
         connectors.add(3);
-        connectors.add(0);
     }
 
-    @Test
-    void printTest() {
-        List<String> screen = new ArrayList<>();
-        // widht = 3*height - 2 | width =  height + i*4
-        int scale = 3; // needs to be odd
-
-        int height = scale;
-        int width = 3*height - 2;
-        String tmpString;
-
-        // Upper border
-        tmpString = "\u250C";
-        for (int i = 1; i < width - 1; i++) {
-            if (i == (width / 2)) {
-                tmpString += THREE_PIPES.ordinal();
-            } else {
-                tmpString += "\u2500";
-            }
-        }
-        tmpString += "\u2510";
-        screen.add(tmpString);
-
-        // Middle
-        for (int i = 1; i < height - 1; i++) {
-            tmpString = "";
-            for (int j = 0; j < width; j++) {
-                if (j == 0 || j == width - 1) {
-                    if (i == height / 2) {
-                        tmpString += THREE_PIPES.ordinal();
-                    } else {
-                        tmpString += "\u2502";
-                    }
-                } else if (j == width / 2 && i == height / 2) {
-                    tmpString += "X";
-                } else {
-                    tmpString += " ";
-                }
-            }
-            screen.add(tmpString);
-        }
-
-        // Lower border
-        tmpString = "\u2514";
-        for (int i = 1; i < width - 1; i++) {
-            if (i == (width / 2)) {
-                tmpString += THREE_PIPES.ordinal();
-            } else {
-                tmpString += "\u2500";
-            }
-        }
-        tmpString += "\u2518";
-        screen.add(tmpString);
-
-        for (String s : screen) {
-            System.out.println(s);
-        }
-    }
+//    @Test
+//    void printTest() {
+//        List<String> screen = new ArrayList<>();
+//        // widht = 3*height - 2 | width =  height + i*4
+//        int scale = 3; // needs to be odd
+//
+//        int height = scale;
+//        int width = 3*height - 2;
+//        String tmpString;
+//
+//        // Upper border
+//        tmpString = "\u250C";
+//        for (int i = 1; i < width - 1; i++) {
+//            if (i == (width / 2)) {
+//                tmpString += THREE_PIPES.ordinal();
+//            } else {
+//                tmpString += "\u2500";
+//            }
+//        }
+//        tmpString += "\u2510";
+//        screen.add(tmpString);
+//
+//        // Middle
+//        for (int i = 1; i < height - 1; i++) {
+//            tmpString = "";
+//            for (int j = 0; j < width; j++) {
+//                if (j == 0 || j == width - 1) {
+//                    if (i == height / 2) {
+//                        tmpString += THREE_PIPES.ordinal();
+//                    } else {
+//                        tmpString += "\u2502";
+//                    }
+//                } else if (j == width / 2 && i == height / 2) {
+//                    tmpString += "X";
+//                } else {
+//                    tmpString += " ";
+//                }
+//            }
+//            screen.add(tmpString);
+//        }
+//
+//        // Lower border
+//        tmpString = "\u2514";
+//        for (int i = 1; i < width - 1; i++) {
+//            if (i == (width / 2)) {
+//                tmpString += THREE_PIPES.ordinal();
+//            } else {
+//                tmpString += "\u2500";
+//            }
+//        }
+//        tmpString += "\u2518";
+//        screen.add(tmpString);
+//
+//        for (String s : screen) {
+//            System.out.println(s);
+//        }
+//    }
 
     @Test
     void printTest_structural() {
-
         Structural structure = new Structural(connectors);
 
         List<String> screen = structure.print();
@@ -96,19 +95,30 @@ public class PrintTest {
 
     @Test
     void printTest_battery() {
-
         Battery battery1 = new Battery(connectors, 2);
         Battery battery2 = new Battery(connectors, 2);
         Battery battery3 = new Battery(connectors, 3);
         Battery battery4 = new Battery(connectors, 3);
+        Battery battery5 = new Battery(connectors, 3);
+
+        Battery emptyBattery2 = new Battery(connectors, 2);
+        Battery emptyBattery3 = new Battery(connectors, 3);
 
         battery2.setAvailability(1);
         battery4.setAvailability(2);
+        battery5.setAvailability(1);
+
+        emptyBattery2.setAvailability(0);
+        emptyBattery3.setAvailability(0);
 
         List<String> screenBattery1 = battery1.print();
         List<String> screenBattery2 = battery2.print();
         List<String> screenBattery3 = battery3.print();
         List<String> screenBattery4 = battery4.print();
+        List<String> screenBattery5 = battery5.print();
+
+        List<String> screenEmptyBattery2 = emptyBattery2.print();
+        List<String> screenEmptyBattery3 = emptyBattery3.print();
 
         for (String s : screenBattery1) {
             System.out.println(s);
@@ -125,11 +135,22 @@ public class PrintTest {
         for (String s : screenBattery4) {
             System.out.println(s);
         }
+
+        for (String s : screenBattery5) {
+            System.out.println(s);
+        }
+
+        for (String s : screenEmptyBattery2) {
+            System.out.println(s);
+        }
+
+        for (String s : screenEmptyBattery3) {
+            System.out.println(s);
+        }
     }
 
     @Test
     void printTest_vital() {
-
         Vital vital_purple = new Vital(connectors, VitalType.PURPLE_VITAL.ordinal());
         Vital vital_brown = new Vital(connectors, VitalType.BROWN_VITAL.ordinal());
 
@@ -143,6 +164,5 @@ public class PrintTest {
         for (String s : screenVital2) {
             System.out.println(s);
         }
-
     }
 }
