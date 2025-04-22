ef5c224c2a116a78957b6d9bab8276d338719987
Filippo <filippo.baldissara@outlook.com>
2025-04-22 14:21:52 +0200

    Created clientEventcards: main abstract class and slavers, others still empty

---
+++ src/main/java/it/polimi/ingsw/is25am28/Client/ClientModel/ClientEventCards/ClientMeteorShower.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.is25am28.Client.ClientModel.ClientEventCards;
+
+public class ClientMeteorShower extends ClientEventCard {
+}
---
+++ src/main/java/it/polimi/ingsw/is25am28/Client/ClientModel/ClientEventCards/ClientSlavers.java
@@ -0,0 +1,61 @@
+package it.polimi.ingsw.is25am28.Client.ClientModel.ClientEventCards;
+
+import it.polimi.ingsw.is25am28.Model.ActionJSON.CardStateJSON;
+import it.polimi.ingsw.is25am28.TUI.WidgetTUI.WidgetTUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientSlavers extends ClientEventCard {
+    private final int requiredFirepower;
+    private final int movementSteps;
+    private final int givenCredits;
+    private final int takenCrew;
+    private final boolean firstRound;
+
+    private List<String> defeatedPlayers;
+
+    public ClientSlavers(CardStateJSON slaversCardState) {
+        super(slaversCardState);
+        this.requiredFirepower = slaversCardState.getRequiredFirepower();
+        this.movementSteps = slaversCardState.getMovementSteps();
+        this.givenCredits = slaversCardState.getGivenCredits();
+        this.takenCrew = slaversCardState.getTakenCrew();
+        this.firstRound = slaversCardState.getFirstRound();
+        this.hasBeenUsed = slaversCardState.getIsCardUsable();
+        this.defeatedPlayers = null;
+    }
+
+    public void useCard() {
+
+    }
+
+    public void updateCard(CardStateJSON slaversCardState) {
+        if (slaversCardState.getFirstRound()) {
+            hasBeenUsed = slaversCardState.getIsCardUsable();
+        } else {
+            defeatedPlayers = slaversCardState.getDefeatedPlayers();
+        }
+    }
+
+    @Override
+    public WidgetTUI generateWidget() {
+        WidgetTUI cardWidget = new WidgetTUI();
+        WidgetTUI cardInfoWidget = new WidgetTUI();
+
+        cardWidget.appendString("====" + this.cardName.toUpperCase() + "====");
+
+        if (this.firstRound) {
+            cardInfoWidget.appendString("Level: " + this.cardLevel);
+            cardInfoWidget.appendString("Given Credits: " + this.givenCredits);
+            cardInfoWidget.appendString("Days: " + this.movementSteps);
+            cardInfoWidget.appendString("Required Firepower: " + this.requiredFirepower);
+            cardInfoWidget.appendString("Taken Crew: " + this.takenCrew);
+        } else {
+            cardInfoWidget.appendString("Player: " + this.playerNickname + " has to give up " + this.takenCrew + " crew members");
+        }
+        cardInfoWidget.wrapWidgetWithBorder();
+
+        return WidgetTUI.composeTwoWidgetsVertically(cardWidget, cardInfoWidget).centerWidgetScreen().wrapWidgetWithBorder();
+    }
+}
