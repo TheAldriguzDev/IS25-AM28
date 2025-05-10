fe1c19bd4e50306246aefffbbb3b44e5f0f5dab0
nanomachine128 <gabriele.bertirotti@mail.polimi.it>
2025-05-10 22:51:48 +0200

    (see prev. commit)

--- src/main/java/it/polimi/ingsw/is25am28/Model/GameModelv2/CardRoundState.java
+++ src/main/java/it/polimi/ingsw/is25am28/Model/GameModelv2/CardRoundState.java
@@ -29,21 +29,22 @@ public final class CardRoundState extends State {
         this.round = 0;
         this.deck = this.model.getGameDeck();
 
-        List<EventCard> AllCards = CardLoader.get().read(model.getBoard(), new ResourceBank(), model.getGameLevel());
-        List<EventCard> fakeDeck = new ArrayList<>();
-        fakeDeck.add(AllCards.get(24)); // OpenSpace
-        fakeDeck.add(AllCards.get(30)); // OpenSpace
-        fakeDeck.add(AllCards.get(38)); // WarZone
-        fakeDeck.add(AllCards.get(0)); // AbandonedShip
-        fakeDeck.add(AllCards.get(5)); // AbandonedStation
-        fakeDeck.add(AllCards.get(8)); // MeteorShower
-        fakeDeck.add(AllCards.get(14)); // Pirates
-        fakeDeck.add(AllCards.get(16)); // VisitPlanets
-        fakeDeck.add(AllCards.get(31)); // Epidemy
-        fakeDeck.add(AllCards.get(32)); // Smugglers
-        fakeDeck.add(AllCards.get(34)); // Slavers
-        fakeDeck.add(AllCards.get(36)); // Stardust
-        this.deck = fakeDeck;
+        // TODO: Remove the fake deck after testing
+//        List<EventCard> AllCards = CardLoader.get().read(model.getBoard(), new ResourceBank(), model.getGameLevel());
+//        List<EventCard> fakeDeck = new ArrayList<>();
+//        fakeDeck.add(AllCards.get(24)); // OpenSpace
+//        fakeDeck.add(AllCards.get(30)); // OpenSpace
+//        fakeDeck.add(AllCards.get(38)); // WarZone
+//        fakeDeck.add(AllCards.get(0)); // AbandonedShip
+//        fakeDeck.add(AllCards.get(5)); // AbandonedStation
+//        fakeDeck.add(AllCards.get(8)); // MeteorShower
+//        fakeDeck.add(AllCards.get(14)); // Pirates
+//        fakeDeck.add(AllCards.get(16)); // VisitPlanets
+//        fakeDeck.add(AllCards.get(31)); // Epidemy
+//        fakeDeck.add(AllCards.get(32)); // Smugglers
+//        fakeDeck.add(AllCards.get(34)); // Slavers
+//        fakeDeck.add(AllCards.get(36)); // Stardust
+//        this.deck = fakeDeck;
 
         this.board = this.model.getBoard();
         this.isFirstState = true;
