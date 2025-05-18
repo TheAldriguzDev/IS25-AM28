a72fb401e707623ba09e970e8f6e4c0b18c50562
Filippo <filippo.baldissara@outlook.com>
2025-05-18 18:08:12 +0200

    WIP on cardRoundCOntroller, Built cardRoundScene mockup

--- src/main/java/it/polimi/ingsw/is25am28/Client/UI/GUI/SceneControllers/CardRoundController.java
+++ src/main/java/it/polimi/ingsw/is25am28/Client/UI/GUI/SceneControllers/CardRoundController.java
@@ -1,4 +1,19 @@
 package it.polimi.ingsw.is25am28.Client.UI.GUI.SceneControllers;
 
+import it.polimi.ingsw.is25am28.Model.ActionJSON.State.CardRoundDTO;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class CardRoundController {
+    @FXML private ImageView cardImageView;
+
+    public void init(CardRoundDTO state) {
+
+    }
+
+    public void setCard() {
+        cardImageView.setImage(new Image("card.png"));
+    }
+
 }
