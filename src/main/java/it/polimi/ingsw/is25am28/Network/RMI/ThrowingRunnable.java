6f5eb245e2ab771ac0eb3d864636d1475e198f8d
teo <teo@teo.it>
2025-06-11 22:01:29 +0200

    Added RMI javadoc

--- src/main/java/it/polimi/ingsw/is25am28/Network/RMI/ThrowingRunnable.java
+++ src/main/java/it/polimi/ingsw/is25am28/Network/RMI/ThrowingRunnable.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.is25am28.Network.RMI;
 
 /**
- * Represents a functional interface that defines a runnable task which can throw exceptions during its execution.
- * It is similar to {@link Runnable} but allows exceptions.
+ * A functional interface representing a runnable task that can throw checked exceptions.
+ * Unlike {@link Runnable}, this interface allows exceptions to be declared and propagated,
+ * enabling cleaner lambda expressions without internal try-catch blocks.
  */
+
 
 @FunctionalInterface
 public interface ThrowingRunnable {
